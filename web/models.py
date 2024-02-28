3f65586e98d49edeec52cc003ca4ca6057253177
Paramount Pictures <nikitos@CodeGenuis>
2024-02-28 16:06:51 +0300

    +models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+Users = get_user_model()
+
+
+class User(models.Model):
+    user_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.CharField(max_length=256)
+
+
+class Transaction(models.Model):
+    transaction_date = models.DateTimeField()
+    type_of_transaction = models.CharField(max_length=256)
+    amount = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    description_category = models.CharField(max_length=256)
+    transaction = models.ManyToOneRel(Transaction)
+
+
+class Task(models.Model):
+    creation_date = models.DateTimeField()
+    status = models.CharField(max_length=256)
+    task_name = models.CharField(max_length=256)
+    task_description = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
