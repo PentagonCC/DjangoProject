fbd91385f12bb2416634988b7a59795e0683f05d
Paramount Pictures <nikitos@CodeGenuis>
2024-03-12 17:34:01 +0300

    complete transaction

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from web.views import main_view, registration_view, auth_view, logout_view, task_edit_view, task_delete_view, \
-    transaction_view
+    transaction_view, transaction_delete_view
 
 urlpatterns = [
     path('', main_view, name="main"),
@@ -12,4 +12,5 @@ urlpatterns = [
     path("tasks/<int:id>", task_edit_view, name="task_edit"),
     path("tasks/<int:id>/delete", task_delete_view, name="task_delete"),
     path("transaction/", transaction_view, name="transaction"),
+    path("transaction/<int:id>", transaction_delete_view, name="transaction_delete"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from web.forms import RegistrationForm, AuthForm, TaskForm, TransactionForm
 from web.models import Task, Transaction
@@ -6,8 +7,9 @@ from web.models import Task, Transaction
 User = get_user_model()
 
 
+@login_required()
 def main_view(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(user=request.user)
     return render(request, 'web/example.html', {
         'tasks': tasks
     })
@@ -50,6 +52,7 @@ def logout_view(request):
     return redirect('main')
 
 
+@login_required
 def task_edit_view(request, id =None):
     task = Task.objects.get(id=id) if id is not None else None
     form = TaskForm(instance=task)
@@ -61,14 +64,28 @@ def task_edit_view(request, id =None):
     return render(request, 'web/task_form.html', {'form': form})
 
 
+@login_required()
 def task_delete_view(request, id):
     task = Task.objects.get(id=id)
     task.delete()
     return redirect("main")
 
 
+@login_required()
 def transaction_view(request):
-    transaction = Transaction.objects.all()
+    transaction = Transaction.objects.filter(user=request.user)
     form = TransactionForm
-    return render(request, 'web/transaction.html', {'transaction': transaction, 'form': form})
+    if request.method == 'POST':
+        form = TransactionForm(data=request.POST, initial={"user": request.user})
+        if form.is_valid():
+            form.save()
+            return redirect('transaction')
+    return render(request, "web/transaction.html", {"transaction": transaction, "form": form})
+
+
+@login_required()
+def transaction_delete_view(request, id):
+    transaction = Transaction.objects.get(id=id)
+    transaction.delete()
+    return redirect("transaction")
 
